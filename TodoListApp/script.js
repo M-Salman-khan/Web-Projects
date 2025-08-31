bef044dd04254c0c6e35b048df13586808ec0bd5
Alejoho <alejoreilly96@gmail.com>
2025-08-31 14:14:07 -0400

    Clean Up and organize script.js File

--- TodoListApp/script.js
+++ TodoListApp/script.js
@@ -1,17 +1,14 @@
 (function (app) {
     'use strict';
 
-    // console.log(currentTime)
-    // let hours = now.getHours()
-    // let minutes = now.getMinutes()
-    // let seconds = now.getSeconds()
     const submitbtn = document.querySelector(".add")
     const userInput = document.querySelector("#userInput")
     const goalTime = document.querySelector("#goalTime")
+
     let id = 0
 
-    app.startUp = function () {
-        submitbtn.addEventListener("click", addTask);
+    function getFormattedTime(date) {
+        return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
     }
 
     function addTask() {
@@ -33,7 +30,7 @@
         <div class="side-3">
                 <button class="dlt" onclick="app.taskDlt(this)">Delete</button>
         </div>`
-        // document.getElementById("time").innerText=`${hours}:${minutes}`
+
         let content = document.querySelector(".content")
         content.append(task)
         userInput.value = ""; // Clear input
@@ -41,11 +38,11 @@
         id++
     }
 
-    function getFormattedTime(date){
-        return `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
-    } 
-
     app.taskDlt = function (button) {
         button.closest(".task").remove()
+    }
+
+    app.startUp = function () {
+        submitbtn.addEventListener("click", addTask);
     }
 })(window.app = window.app || {});
